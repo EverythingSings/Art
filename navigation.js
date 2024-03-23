cb26657099ffddc14aefe9c4d95baf4ad84678cd
EverythingSings <EverythingSings@protonmail.com>
2024-03-22 19:35:45 -0600

    dom parser experiment

--- navigation.js
+++ navigation.js
@@ -1,8 +1,12 @@
-// Fetch the pages data from pages.json
-fetch('pages.json')
-  .then(response => response.json())
-  .then(data => {
-    const pages = data.pages;
+// Fetch the list of HTML files from the server
+fetch('.')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const links = doc.querySelectorAll('a[href$=".html"]');
+    const pages = Array.from(links).map(link => link.href);
+
     const dropdownMenu = document.querySelector('.dropdown-menu');
 
     // Populate the dropdown menu with page links
@@ -15,3 +19,6 @@ fetch('pages.json')
       dropdownMenu.appendChild(li);
     });
   })
+  .catch(error => {
+    console.error('Error fetching HTML files:', error);
+  });
