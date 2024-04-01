3861e1a5077f10afeb53725e364315c9b30b41bb
EverythingSings <EverythingSings@protonmail.com>
2024-03-31 23:47:49 -0600

    puter chat changes

--- chat.js
+++ chat.js
@@ -6,7 +6,7 @@ function initializeChatPage() {
 
   if (chatButton) {
     chatButton.addEventListener('click', () => {
-      const question = chatInput.value;
+      const question = "You are a chatbot on the website EverythingSings.Art, The formless art brand building for those oriented towards a rapidly changing future. Try to share as much inspiration and love as possible. " + chatInput.value;
       puter.ai.chat(question).then((response) => {
         chatResponse.textContent = response;
       });
